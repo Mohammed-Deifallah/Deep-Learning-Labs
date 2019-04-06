d3872c8f2d3ad3c0b66f2c50473ce51fd55db141
Mohammed Deifallah <mohammed_deifallah@yahoo.com>
2019-04-06 11:40:59 +0200

    commit

---
+++ Lab3/assignment3.py
@@ -0,0 +1,81 @@
+import numpy as np
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+%matplotlib inline
+
+def HappyModel(input_shape):
+    """
+    Implementation of the HappyModel.
+    
+    Arguments:
+    input_shape -- shape of the images of the dataset
+
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    
+    ### START CODE HERE ###
+
+    ### END CODE HERE ###
+    
+    return model
+
+def load_dataset():
+    path_to_train = "assignment3_train.h5"
+    path_to_test = "assignment3_test.h5"
+    train_dataset = h5py.File(path_to_train)
+    train_x = np.array(train_dataset['train_set_x'][:])
+    train_y = np.array(train_dataset['train_set_y'][:])
+
+    test_dataset = h5py.File(path_to_test)
+    test_x = np.array(test_dataset['test_set_x'][:])
+    test_y = np.array(test_dataset['test_set_y'][:])
+
+    # y reshaped
+    train_y = train_y.reshape((1, train_x.shape[0]))
+    test_y = test_y.reshape((1, test_y.shape[0]))
+
+    return train_x, train_y, test_x, test_y  
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+#Create, compile and fit the model
+### START CODE HERE ### 
+
+### END CODE HERE ###
+
+# Evaluate your model.
+### START CODE HERE ### 
+
+### END CODE HERE ###
